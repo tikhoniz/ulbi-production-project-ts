6611ebf2b5505ca5147e0d8ba5524dff1e1293ef
tikhoniz <tikhonbright@gmail.com>
2023-09-01 01:07:23 +0700

    fix page AboutPageAsync

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -2,9 +2,10 @@ import { lazy } from 'react'
 
 export const AboutPageAsync = lazy(
   async () =>
-    new Promise(resolve => {
-    // @ts-ignore
-  // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-      setTimeout(() => resolve(import('./AboutPage')), 1500);
-		})
+    await new Promise(resolve => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+      setTimeout(() => resolve(import('./AboutPage')), 1500)
+    })
 )
