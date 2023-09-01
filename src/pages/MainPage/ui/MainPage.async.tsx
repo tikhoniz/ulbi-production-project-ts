8f93de804ed095958ccb3578d6a5bc42738826e2
tikhoniz <tikhonbright@gmail.com>
2023-09-01 15:30:32 +0700

    fixed MainPage

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,11 @@
 import { lazy } from 'react'
 
-export const MainPageAsync = lazy(() => new Promise(resolve => {
-  // @ts-ignore
-  // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-  setTimeout(() => resolve(import('./MainPage')), 1500)
-}))
+export const MainPageAsync = lazy(
+  async () =>
+    await new Promise(resolve => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+      setTimeout(() => resolve(import('./MainPage')), 1500)
+    })
+)
