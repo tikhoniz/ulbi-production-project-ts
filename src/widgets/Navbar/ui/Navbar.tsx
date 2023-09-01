3bda0d9a6607ec673fbec28b2b1e4a498d8e0beb
tikhoniz <tikhonbright@gmail.com>
2023-09-01 18:12:32 +0700

    test chromatic

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,7 @@
+import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classnames'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -7,15 +9,22 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps): JSX.Element => {
+  const { t } = useTranslation()
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.liks}></div>
       <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
-        Main
+        {t('Main')}
       </AppLink>
       <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-        About
+        {t('About')}
       </AppLink>
+      <Button
+        className={classNames('', {}, [className])}
+        theme={ThemeButton.CLEAR}
+      >
+        {t('lang')}
+      </Button>
     </div>
   )
 }
