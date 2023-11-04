7d1e3ddb8142d738c0a11307a832197bd11e6d79
tikhoniz <tikhonbright@gmail.com>
2023-11-04 13:47:24 +0700

    fix: api  config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,8 +5,18 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorge'
 
 // в baseURL указываем порт на котором бекэнд
 export const $api = axios.create({
-  baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY)
+  baseURL: __API__
+})
+
+/* необходимо добавить перехватчик, поскольку мы пользователя достаем из локалстораджа в момент создания инстанса, и когда мы авторизовались, инстанс уже был создан и даже после авторизации в строке authorization остается пустая строка */
+
+/* interceptors отрабатывает перед каким-то действием */
+
+$api.interceptors.request.use((config) => {
+  // добавляем guard
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY)
   }
+
+  return config
 })
